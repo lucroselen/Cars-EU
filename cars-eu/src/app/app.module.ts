c8352ef3d544d50dd91b94109ccebe1a9bf5ede2
lucroselen <mriliev@outlook.com>
2022-03-21 20:40:40 +0200

    footer created

--- cars-eu/src/app/app.module.ts
+++ cars-eu/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, HomepageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, FooterComponent, HeaderComponent],
 })
-export class AppModule { }
+export class AppModule {}
