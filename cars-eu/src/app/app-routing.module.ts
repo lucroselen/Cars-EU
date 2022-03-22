b8dcd88f9cfb09c243cd92f8f5dbe3dd11d8ec3a
lucroselen <mriliev@outlook.com>
2022-03-22 02:52:00 +0200

    generate emtpy component

--- cars-eu/src/app/app-routing.module.ts
+++ cars-eu/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCarComponent } from './add-car/add-car.component';
+import { AllCarsComponent } from './all-cars/all-cars.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
+import { EditCarComponent } from './edit-car/edit-car.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 import { MapsComponent } from './maps/maps.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -13,6 +20,34 @@ const routes: Routes = [
   {
     path: 'find-us',
     component: MapsComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'all-cars',
+    component: AllCarsComponent,
+  },
+  {
+    path: 'details',
+    component: CarDetailsComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'add',
+    component: AddCarComponent,
+  },
+  {
+    path: 'edit',
+    component: EditCarComponent,
   },
   {
     path: '**',
--- cars-eu/src/app/app.module.ts
+++ cars-eu/src/app/app.module.ts
@@ -8,6 +8,13 @@ import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MapsComponent } from './maps/maps.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AddCarComponent } from './add-car/add-car.component';
+import { EditCarComponent } from './edit-car/edit-car.component';
+import { AllCarsComponent } from './all-cars/all-cars.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +24,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
     HomepageComponent,
     MapsComponent,
     NotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddCarComponent,
+    EditCarComponent,
+    AllCarsComponent,
+    CarDetailsComponent,
+    ProfileComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
