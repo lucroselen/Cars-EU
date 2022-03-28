f6360861bff81d46e4e1d557d65329453a9a9325
lucroselen <mriliev@outlook.com>
2022-03-29 02:20:17 +0300

    add car component added and styled

--- cars-eu/src/app/add-car/add-car.component.ts
+++ cars-eu/src/app/add-car/add-car.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.ShadowDom,
   selector: 'app-add-car',
   templateUrl: './add-car.component.html',
-  styleUrls: ['./add-car.component.css']
+  styleUrls: ['./add-car.component.css'],
 })
 export class AddCarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
