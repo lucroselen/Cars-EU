89ec2c8eed1c60438e316deb5992c5ba3dd15233
lucroselen <mriliev@outlook.com>
2022-03-29 03:04:01 +0300

    profile page ready

--- cars-eu/src/app/profile/profile.component.ts
+++ cars-eu/src/app/profile/profile.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.ShadowDom,
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
