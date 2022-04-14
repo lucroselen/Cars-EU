ac61c6fe3cb5d46cdda4defaadad0218e5af605c
lucroselen <mriliev@outlook.com>
2022-04-15 02:59:51 +0300

    car details implemented and set

--- cars-eu/src/app/car-details/car-details.component.ts
+++ cars-eu/src/app/car-details/car-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarService } from '../car.service';
 
 @Component({
   encapsulation: ViewEncapsulation.ShadowDom,
@@ -7,7 +9,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./car-details.component.css'],
 })
 export class CarDetailsComponent implements OnInit {
-  constructor() {}
+  public car: object;
+  public voted: object;
+  public isOwnedBy: object;
+  public isInFavorites: object;
+  public isLogged: any;
+  constructor(public carService: CarService, public route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isLogged = localStorage.getItem('id');
+    let id = this.route.snapshot.params['id'];
+    this.carService.getOne$(id).subscribe((e) => {
+      this.car = e['car'];
+      this.voted = e['voted'];
+      this.isOwnedBy = e['isOwnedBy'];
+      this.isInFavorites = e['isInFavorites'];
+    });
+  }
 }
