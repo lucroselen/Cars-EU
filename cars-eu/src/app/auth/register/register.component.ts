9504793e951afc55d32c83f1f6143455b30b705c
lucroselen <mriliev@outlook.com>
2022-04-19 02:08:01 +0300

    register validations added

--- cars-eu/src/app/auth/register/register.component.ts
+++ cars-eu/src/app/auth/register/register.component.ts
@@ -8,6 +8,7 @@ import {
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth.service';
 import { IUser } from 'src/app/core/interfaces/user';
+import { NotificationsService } from 'src/app/core/notifications.service';
 
 @Component({
   selector: 'app-register',
@@ -19,26 +20,36 @@ export class RegisterComponent implements OnInit {
     firstName: new FormControl(null, [
       Validators.required,
       Validators.minLength(2),
+      Validators.pattern(/^[A-Z][a-z]*$/),
     ]),
     lastName: new FormControl(null, [
       Validators.required,
       Validators.minLength(2),
+      Validators.pattern(/^[A-Z][a-z]*$/),
     ]),
-    email: new FormControl(null, [Validators.required]),
-    password: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(2),
-    ]),
+    email: new FormControl(
+      null,
+      Validators.compose([Validators.required, Validators.email])
+    ),
+    password: new FormControl(
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/^[^ ]*$/),
+      ])
+    ),
     rePassword: new FormControl(null, [
       Validators.required,
-      Validators.minLength(2),
+      Validators.minLength(6),
     ]),
   });
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private notifications: NotificationsService
   ) {}
 
   ngOnInit(): void {}
@@ -46,8 +57,52 @@ export class RegisterComponent implements OnInit {
     const { firstName, lastName, email, password, rePassword } =
       this.registerFormGroup.value;
 
+    //front-end validation
+    if (
+      this.registerFormGroup.get('email')?.hasError('required') ||
+      this.registerFormGroup.get('password')?.hasError('required') ||
+      this.registerFormGroup.get('rePassword')?.hasError('required') ||
+      this.registerFormGroup.get('firstName')?.hasError('required') ||
+      this.registerFormGroup.get('lastName')?.hasError('required')
+    ) {
+      this.notifications.showError('All fields are required!');
+      return;
+    }
+    if (this.registerFormGroup.get('firstName')?.hasError('pattern')) {
+      this.notifications.showError(
+        `${firstName} is not a valid first name! Use only latin letters and no white spaces!`
+      );
+      return;
+    }
+    if (this.registerFormGroup.get('lastName')?.hasError('pattern')) {
+      this.notifications.showError(
+        `${lastName} is not a valid last name! Use only latin letters and no white spaces!`
+      );
+      return;
+    }
+    if (this.registerFormGroup.get('email')?.hasError('email')) {
+      this.notifications.showError('Email address is incorrect format!');
+      return;
+    }
+    if (this.registerFormGroup.get('password')?.hasError('minlength')) {
+      this.notifications.showError(
+        'Password should be at least 6 characters long!'
+      );
+      return;
+    }
+    if (
+      this.registerFormGroup.get('password')?.hasError('pattern') ||
+      this.registerFormGroup.get('rePassword')?.hasError('pattern')
+    ) {
+      this.notifications.showError('Password cannot contain white spaces!');
+      return;
+    }
+    if (password != rePassword) {
+      this.notifications.showError('Both passwords should be the same!');
+      return;
+    }
+
     const body: IUser = {
-      id: '',
       firstName,
       lastName,
       email,
@@ -58,9 +113,12 @@ export class RegisterComponent implements OnInit {
       next: (e: any) => {
         localStorage.setItem('id', e.id);
         this.authService.authenticate().subscribe();
-      },
-      complete: () => {
         this.router.navigate(['/all-cars']);
+        this.notifications.showSuccess('Registration successful!');
+      },
+      error: (err) => {
+        //back-end validation
+        this.notifications.showError(err.error.error);
       },
     });
   }
